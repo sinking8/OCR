d2e5de5a17966d9cea59fa788433ec40e03b8711
sinking8 <ashwinblaze111@gmail.com>
2020-12-08 22:04:19 +0530

    updated

--- project/model.py
+++ project/model.py
@@ -10,7 +10,12 @@ class Detect:
 	def __init__(self,img):
 		self.img = img
 		#self.detect_model = keras.models.load_model('./project/detect.h5',compile=False)
-		self.lenet_model  = keras.models.load_model('./project/Lenet.h5',compile=False)
+		try:
+			self.lenet_model  = keras.models.load_model('./project/Lenet.h5',compile=False)
+
+		except:
+			self.lenet_model  = keras.models.load_model('Lenet.h5',compile=False)
+
 
 	def detect_text(self):
 		text  =  self.detect_license_plate()
@@ -35,7 +40,6 @@ class Detect:
 			#Detecting Contours
 			contours,_ = cv2.findContours(edged, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 			code = ''
-
 
 			if(len(contours) !=0):
 			    for c in contours:
